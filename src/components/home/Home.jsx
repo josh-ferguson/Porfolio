6ba3ae41a21524184ac12db15088f5c8a0022430
josh-ferguson <joshferguso@hotmail.co.uk>
2021-10-28 21:00:43 +0100

    Home section added -issues with particles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,48 @@
 import "./home.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
+import Particles from "react-particles-js";
 
 export default function Home() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            strings: ["UX Design", "Website Development", "Databases" ] 
+        });
+    }, [])
+
     return (
         <div className="home" id="home">
-            
+            <Particles
+                params={{
+                    particles: {
+                        number: {
+                            value: 35,
+                            density: {
+                                enable: true,
+                                value_area: 900
+                            }
+                        }
+                    }
+                }}
+            />
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/profile pic.jpg" alt="Profile picture" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Josh Ferguson</h1>
+                    <h3>
+                        . <span ref={textRef}></span>
+                    </h3>
+                </div>
+            </div>
         </div>
     )
 }
